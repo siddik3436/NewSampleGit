51031bd78686547cb99c10fd53c059e36117b4a8
siddik3436 <siddik3436@gmail.com>
2015-03-20 18:05:24 +0600

    add second activity

--- SampleTestGitProject/gen/com/example/sampletestgitproject/R.java
+++ SampleTestGitProject/gen/com/example/sampletestgitproject/R.java
@@ -1750,7 +1750,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_second=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1815,6 +1816,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a0010;
         public static final int hello_world=0x7f0a0011;
+        public static final int title_activity_second=0x7f0a0013;
     }
     public static final class style {
         /** 
